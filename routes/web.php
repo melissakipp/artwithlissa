44b09a09284837a1d7fd6e67208cfbd3a673b735
Melissa Kipp <melissakipp.az@gmail.com>
2022-12-30 14:19:07 -0700

    Feature Notes DB, CRUD

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\NoteController;
 
 /*
 |--------------------------------------------------------------------------
@@ -28,4 +29,8 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+
+Route::resource('/notes', NoteController::class)->middleware(['auth']);
+
+
+require __DIR__ . '/auth.php';
