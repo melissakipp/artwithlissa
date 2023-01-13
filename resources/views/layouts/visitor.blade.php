210f96512faf8e3b3f526da425de3889b18cd1d0
Melissa Kipp <melissakipp.az@gmail.com>
2023-01-13 05:56:36 -0700

    empty - kept git history

--- resources/views/layouts/visitor.blade.php
+++
@@ -1,6 +0,0 @@
-<nav class="customer-menu">
-    <ul class="customer-menu__list">
-        <li class="customer-menu__item"><a href="/">Home</a></li>
-        <li class="customer-menu__item"><a href="/blog">Blog</a></li>
-    </ul>
-</nav>
