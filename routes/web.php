ae5b4a96a4d5c5dc08e941478e39c9c4de7b6e60
Melissa Kipp <melissakipp.az@gmail.com>
2023-01-13 05:38:00 -0700

    restarting

--- routes/web.php
+++ routes/web.php
@@ -40,7 +40,7 @@ Route::resource('/blog', PostController::class);
 
 // Route Grouping
 Route::prefix('/trashed')->name('trashed.')->middleware('auth')->group(function () {
-    // Route::get('/', [TrashedNoteController::class, 'index'])->name('index');
+     Route::get('/', [TrashedNoteController::class, 'index'])->name('index');
     Route::get('/{note}', [TrashedNoteController::class, 'show'])->name('show')->withTrashed();
     Route::put('/{note}', [TrashedNoteController::class, 'update'])->name('update')->withTrashed();
     Route::delete('/{note}', [TrashedNoteController::class, 'destroy'])->name('destroy')->withTrashed();
